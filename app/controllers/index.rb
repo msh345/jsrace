c7792acc8e75f57e18440478f8161b9922588e62
Ryan Kelley <rskelley9@gmail.com>
2013-09-24 19:15:03 -0500

    updated index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,7 +4,6 @@ get '/' do
   erb :index
 end
 
-
 get '/winner' do
   @games = Game.all
   erb :winner
@@ -12,38 +11,44 @@ end
 
 get '/game/:id' do
   @game = Game.find(params[:id])
-  erb :old_game
+  @player1 = @game.players.first
+  @player2 = @game.players.last
+  erb :game
 end
 
+get '/game/:id/old_game' do
+  @game = Game.find(params[:id])
+  erb :old_game
+end
 
 
 
 ##################POST######################
 
 post '/game' do
-  @game = Game.new
+  game = Game.new
 
   if params.has_blank?
     @error_message = "Please enter both players' names"
   else
-    @player1 = Player.find_or_create_by_name(params[:player1])
-    @player2 = Player.find_or_create_by_name(params[:player2])
+    player1 = Player.find_or_create_by_name(params[:player1])
+    player2 = Player.find_or_create_by_name(params[:player2])
   end
 
-  @game.players << [@player1,@player2] #game.players += [@player1,@player2]
+  game.players << [player1,player2] #game.players += [@player1,@player2]
 
-  erb(:game) if @game.save
-
-  erb :index
-
+  if game.save
+    redirect to "/game/#{game.id}"
+  else
+    redirect back
+  end
 end
 
 post '/winner' do
   if request.xhr?
+    # content_type :json
     game = Game.find(params[:game_id])
-
     game.update_attributes(winner: params[:winner], time: params[:time])
-    @games = Game.all
+    # {status: 200}.to_json
   end
-  erb :winner
 end
